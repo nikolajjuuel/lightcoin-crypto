f03559a8211709548f9db3717febc868c91c1da4
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-09-28 18:20:45 +0000

    promises added

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-let balance = 500.00;
+class Deposit {
+
+  // Pass in the account that the deposit this for
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  // Update the balance in the account
+  commit() {
+    this.account.balance += this.amount;
+  }
+
+}
 
 class Withdrawal {
 
-  constructor(amount) {
+  // Pass in the account that the withdrawal this for
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
+  // Update the balance in the account
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
+  }
+
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
   }
 
 }
 
 
-
+const myAccount = new Account("snow-patrol");
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
 
 console.log('Balance:', balance);
